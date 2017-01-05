5112a968c15a54610a3ad238b9e2b3b897b1cdb0
Siva Boyapati <siva.boyapati@autodesk.com>
2017-01-04 21:58:29 -0800

    gmail clone

--- NodeJS Tutorials/Tutorial  17 - Issue And Expire Tokens/config/auth.js
+++ NodeJS Tutorials/Tutorial  17 - Issue And Expire Tokens/config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
+		'clientID': '1741920626023567',
+		'clientSecret': 'a73c194fa56034db4b3daf8a0b290f9e',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
--- NodeJS Tutorials/Tutorial 12 - Combining Accounts/config/auth.js
+++ NodeJS Tutorials/Tutorial 12 - Combining Accounts/config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
-	'facebookAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
-	},
+    'facebookAuth' : {
+        'clientID': '1729225200699121',
+        'clientSecret': '39710af415bd0c6456f2afd14377726a',
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    },
 
-	'googleAuth' : {
-		'clientID': 'enter client id here',
-		'clientSecret': 'enter client secret here',
-		'callbackURL': 'enter callback here'
-	}
+    'googleAuth' : {
+        'clientID': 'enter client id here',
+        'clientSecret': 'enter client secret here',
+        'callbackURL': 'enter callback here'
+    }
 }
